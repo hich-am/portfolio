5521e66922643fa3c5faae805eb6fa2c9732e4a8
hicham manseur <nh_manseur@esi.dz>
2025-06-01 09:26:08 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        viridian: {
+          50: '#E6ECEA',
+          600: '#2F5F4D',
+          800: '#40826D',
+        },
+        phthalo: {
+          800: '#123524',
+          900: '#0A1C18',
+        },
+      },
+    },
+  },
+};
